7476a0338ab3316060d4587e3736598939601dc8
Scott Bartell <me@scottbartell.com>
2016-08-04 11:25:03 -0400

    refactor to use RequestBodyGenerator as the class to represent

--- lib/vantiv/api/request_body_generator.rb
+++ lib/vantiv/api/request_body_generator.rb
@@ -2,29 +2,25 @@ require 'securerandom'
 
 module Vantiv
   module Api
-    class RequestBodyA < OpenStruct; end
     class Card < OpenStruct; end
     class Transaction < OpenStruct; end
     class PaymentAccount < OpenStruct; end
 
     class RequestBodyGenerator
+      attr_reader :acceptor_id, :application_id, :report_group, :card, :transaction, :payment_account
+
       def initialize(card: nil, transaction: nil, payment_account: nil)
         @card = card
         @transaction = transaction
         @payment_account = payment_account
+
+        @acceptor_id = Vantiv.acceptor_id
+        @application_id = SecureRandom.hex(12)
+        @report_group = Vantiv.default_report_group
       end
 
       def run
-        request_body = RequestBodyA.new(
-          acceptor_id: Vantiv.acceptor_id,
-          application_id: SecureRandom.hex(12),
-          report_group: Vantiv.default_report_group,
-          card: @card,
-          transaction: @transaction,
-          payment_account: @payment_account
-        )
-
-        ::RequestBodyGeneratorRepresenter.new(request_body).to_hash
+        ::RequestBodyGeneratorRepresenter.new(self).to_hash
       end
     end
   end
