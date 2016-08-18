c578211ee8bfebded1db59e38c2f97418d3c354f
Scott Bartell <me@scottbartell.com>
2016-08-18 16:13:34 -0400

    remove print statement

--- lib/vantiv/certification/response_cache.rb
+++ lib/vantiv/certification/response_cache.rb
@@ -13,7 +13,6 @@ module Vantiv
         cert_name = values_tree.shift
         response = @responses[cert_name]
         method_chain = values_tree.join('.')
-        p response
         response.instance_eval(method_chain)
       end
     end
