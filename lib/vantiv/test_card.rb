77346f112465d02654a96b2ded9ffb1072698125
Rick Martínez <rick@plated.com>
2017-01-02 17:56:10 -0500

    Base test card expiration year on today's date

--- lib/vantiv/test_card.rb
+++ lib/vantiv/test_card.rb
@@ -1,13 +1,18 @@
 module Vantiv
   class TestCard
     class CardNotFound < StandardError; end
+    THIS_YEAR = Date.today.strftime("%y")
+    LAST_YEAR = (Date.today << 12).strftime("%y")
+    FOUR_YEARS_FROM_NOW = (Date.today >> 48).strftime("%y")
+    LAST_CENTURY = "99"
+
     CARDS = [
       {
         access_method_name: "valid_account",
         attrs: {
           card_number: "4457010000000009",
           expiry_month: "01",
-          expiry_year: "21",
+          expiry_year: THIS_YEAR,
           cvv: "349",
           mocked_sandbox_payment_account_id: "1111111111110009",
           network: "VI"
@@ -18,7 +23,7 @@ module Vantiv
         attrs: {
           card_number: "4457010010900010",
           expiry_month: "01",
-          expiry_year: "21",
+          expiry_year: THIS_YEAR,
           cvv: "349",
           mocked_sandbox_payment_account_id: nil,
           network: "VI"
@@ -29,7 +34,7 @@ module Vantiv
         attrs: {
           card_number: "5112001600000006",
           expiry_month: "01",
-          expiry_year: "20",
+          expiry_year: FOUR_YEARS_FROM_NOW,
           cvv: "349",
           mocked_sandbox_payment_account_id: "1111111111130006",
           network: "MC"
@@ -40,7 +45,7 @@ module Vantiv
         attrs: {
           card_number: "4457002100000005",
           expiry_month: "01",
-          expiry_year: "20",
+          expiry_year: FOUR_YEARS_FROM_NOW,
           cvv: "349",
           mocked_sandbox_payment_account_id: "1111111111120005",
           network: "VI"
@@ -51,7 +56,7 @@ module Vantiv
         attrs: {
           card_number: "5112001900000003",
           expiry_month: "01",
-          expiry_year: "20",
+          expiry_year: FOUR_YEARS_FROM_NOW,
           cvv: "349",
           mocked_sandbox_payment_account_id: "1111111111140003",
           network: "MC"
@@ -62,7 +67,7 @@ module Vantiv
         attrs: {
           card_number: "4457000300000007",
           expiry_month: "01",
-          expiry_year: "15",
+          expiry_year: LAST_YEAR,
           cvv: "123",
           mocked_sandbox_payment_account_id: "1111111111120007",
           network: "VI"
@@ -73,7 +78,7 @@ module Vantiv
         attrs: {
           card_number: "5112000101110009",
           expiry_month: "11",
-          expiry_year: "99",
+          expiry_year: LAST_CENTURY,
           cvv: "123",
           mocked_sandbox_payment_account_id: "1111111111160009",
           network: "MC"
@@ -84,7 +89,7 @@ module Vantiv
         attrs: {
           card_number: "4457000301100004",
           expiry_month: "11",
-          expiry_year: "99",
+          expiry_year: LAST_CENTURY,
           cvv: "123",
           mocked_sandbox_payment_account_id: "1111111111130004",
           network: "VI"
