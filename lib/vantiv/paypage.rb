5bda4cf4093283e2c5e19e554272ff71d9869798
Aaron Cohen <aaron.cohen@plated.com>
2017-02-16 18:53:29 -0500

    Add script url for paypage api

--- lib/vantiv/paypage.rb
+++ lib/vantiv/paypage.rb
@@ -1,5 +1,13 @@
 module Vantiv
   module Paypage
+    def self.api_js
+      if Vantiv::Environment.production?
+        "https://request.securepaypage-litle.com/LitlePayPage/litle-api2.js"
+      else
+        "https://request-prelive.np-securepaypage-litle.com/LitlePayPage/litle-api2.js"
+      end
+    end
+
     def self.payframe_js
       if Vantiv::Environment.production?
         "https://request.securepaypage-litle.com/LitlePayPage/js/payframe-client.min.js"
