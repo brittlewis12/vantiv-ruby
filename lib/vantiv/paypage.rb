6058d18951d5010066f4c9e82fc53c15a6a0694e
Aaron Cohen <aaron.cohen@plated.com>
2017-08-01 16:47:55 -0400

    [PF-382] Remove deprecated  litle urls

--- lib/vantiv/paypage.rb
+++ lib/vantiv/paypage.rb
@@ -1,51 +1,25 @@
 module Vantiv
   module Paypage
     def self.root_uri
-      if use_vantiv_rebrand_urls?
-        if Vantiv::Environment.production?
-          "https://request.eprotect.vantivcnp.com"
-        elsif Vantiv::Environment.postcertification?
-          "https://request.eprotect.vantivpostlive.com"
-        else
-          "https://request.eprotect.vantivprelive.com"
-        end
+      if Vantiv::Environment.production?
+        "https://request.eprotect.vantivcnp.com"
+      elsif Vantiv::Environment.postcertification?
+        "https://request.eprotect.vantivpostlive.com"
       else
-        if Vantiv::Environment.production?
-          "https://request.securepaypage-litle.com"
-        elsif Vantiv::Environment.postcertification?
-          "https://request-postlive.np-securepaypage-litle.com"
-        else
-          "https://request-prelive.np-securepaypage-litle.com"
-        end
+        "https://request.eprotect.vantivprelive.com"
       end
     end
 
     def self.api_js
-      if use_vantiv_rebrand_urls?
-        "#{root_uri}/eProtect/litle-api2.js"
-      else
-        "#{root_uri}/LitlePayPage/litle-api2.js"
-      end
+      "#{root_uri}/eProtect/litle-api2.js"
     end
 
     def self.jquery_js
-      if use_vantiv_rebrand_urls?
-        "#{root_uri}/eProtect/js/jquery-1.11.2.min.js"
-      else
-        "#{root_uri}/LitlePayPage/js/jquery-1.11.2.min.js"
-      end
+      "#{root_uri}/eProtect/js/jquery-1.11.2.min.js"
     end
 
     def self.payframe_js
-      if use_vantiv_rebrand_urls?
-        "#{root_uri}/eProtect/js/payframe-client.min.js"
-      else
-        "#{root_uri}/LitlePayPage/js/payframe-client.min.js"
-      end
-    end
-
-    private_class_method def self.use_vantiv_rebrand_urls?
-      !ENV.fetch("ENABLE_VANTIV_LITLE_MIGRATION", "").empty?
+      "#{root_uri}/eProtect/js/payframe-client.min.js"
     end
   end
 end
